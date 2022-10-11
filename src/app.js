a7cd4da09d46a01f3633e9e998df4b65cc333adb
Young <navskh@naver.com>
2022-10-11 19:47:34 +0900

    morgan update

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import Router from './routes/index.js';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const corsOptions = {
 	origin: '*',
